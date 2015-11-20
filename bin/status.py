98eab790fb175c907dc299976b7407663a62f282
Mike Walker <mwalker@mwalker-mbpr15.local>
2015-11-20 14:53:29 -0500

    first iteration of scripted input

---
+++ bin/status.py
@@ -0,0 +1,14 @@
+import subprocess
+import json
+
+data = {}
+
+result = subprocess.check_output("/usr/bin/apcaccess")
+for line in result.split('\n'):
+    (key, spl, value) = line.partition(': ')
+    key = key.rstrip().lower()
+    value = value.strip()
+    value = value.split(' ', 1)[0]
+    data[key] = value
+
+print json.dumps(data, indent=4, sort_keys=True)
